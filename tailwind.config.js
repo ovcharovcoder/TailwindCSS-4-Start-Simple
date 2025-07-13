8773976af69cae2c36e6e37bd90d0944dd4ac691
Andriy Ovcharov <ovcharovcoder@gmail.com>
2025-07-13 06:56:56 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./*.html', './js/*.{js,ts,jsx,tsx}'],
+  content: ['./src/*.html', './src/js/*.{js,ts,jsx,tsx}'],
   safelist: [...],
   theme: {
     extend: {
